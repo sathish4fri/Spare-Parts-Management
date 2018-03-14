47cbd40a4b6397fded49054e21250f1847c58a34
Deepan <mkdeepan@gmail.com>
2018-03-14 06:39:41 +0530

    lastest db and refer postman json file for api req resp by importing it in postman

--- mydb.sql
+++ mydb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 03, 2018 at 12:52 PM
+-- Generation Time: Mar 13, 2018 at 11:02 AM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.20
 
@@ -29,12 +29,19 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `area` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `area_name` varchar(45) DEFAULT NULL,
-  `status` enum('0','1') DEFAULT NULL,
+  `status` enum('0','1') DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_delete` enum('0','1') DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `area`
+--
+
+INSERT INTO `area` (`id`, `area_name`, `status`, `created_at`, `updated_at`, `is_delete`) VALUES
+(1, 'vellore', '1', NULL, NULL, '0');
 
 -- --------------------------------------------------------
 
@@ -48,13 +55,20 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `emp_lastname` varchar(45) DEFAULT NULL,
   `designation` varchar(45) DEFAULT NULL,
   `emp_id` varchar(45) NOT NULL,
-  `status` enum('0','1') DEFAULT NULL,
+  `status` enum('0','1') DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_delete` enum('0','1') DEFAULT '0',
   PRIMARY KEY (`eid`),
   UNIQUE KEY `emp_id_UNIQUE` (`emp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`eid`, `emp_firstname`, `emp_lastname`, `designation`, `emp_id`, `status`, `created_at`, `updated_at`, `is_delete`) VALUES
+(1, 'Deepan', 'K', 'Analyst', '588840', '1', NULL, NULL, '0');
 
 -- --------------------------------------------------------
 
@@ -68,11 +82,21 @@ CREATE TABLE IF NOT EXISTS `issue_list` (
   `quantity` double DEFAULT NULL,
   `ticket_no` varchar(45) DEFAULT NULL,
   `emp_id` varchar(45) DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `is_delete` enum('0','1') DEFAULT '0',
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `issue_list`
+--
+
+INSERT INTO `issue_list` (`id`, `item_id`, `quantity`, `ticket_no`, `emp_id`, `is_delete`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, 1, 5, '544450', '1', '0', 1, 1, NULL, NULL),
+(2, 1, 2, '588840', '1', '0', 1, 1, '2018-03-13 06:30:00', '2018-03-13 05:29:58');
 
 -- --------------------------------------------------------
 
@@ -83,13 +107,21 @@ CREATE TABLE IF NOT EXISTS `issue_list` (
 CREATE TABLE IF NOT EXISTS `items` (
   `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_name` varchar(45) DEFAULT NULL,
-  `status` enum('0','1') DEFAULT NULL,
+  `status` enum('0','1') DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_delete` enum('0','1') DEFAULT '0',
   `remarks` text,
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `item_name`, `status`, `created_at`, `updated_at`, `is_delete`, `remarks`) VALUES
+(1, 'RAM', '1', NULL, NULL, '0', 'fuck'),
+(2, 'Intel Processor', '1', '2018-03-12 18:30:00', '2018-03-13 04:45:48', '0', 'yes its good');
 
 -- --------------------------------------------------------
 
@@ -106,11 +138,21 @@ CREATE TABLE IF NOT EXISTS `purchase` (
   `warranty_period` varchar(45) DEFAULT NULL,
   `invoice_no` varchar(45) DEFAULT NULL,
   `serial_no` varchar(45) DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `is_delete` enum('0','1') DEFAULT '0',
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`pur_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `purchase`
+--
+
+INSERT INTO `purchase` (`pur_id`, `item_id`, `vendor`, `specification`, `quantity`, `warranty_period`, `invoice_no`, `serial_no`, `is_delete`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, 1, 'm/s venus electronics', 'i3', 2, '4', '12345', '24234234234', '0', 1, 1, '2018-03-12 18:30:00', '2018-03-13 04:36:42'),
+(2, 1, 'venus electronics', 'i3', 2, '4', '12345', '24234234234', '0', 1, 1, '2018-03-12 18:30:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -123,9 +165,9 @@ CREATE TABLE IF NOT EXISTS `reorder` (
   `area_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL,
   `level_count` int(11) DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `is_delete` enum('0','1') DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -139,11 +181,11 @@ CREATE TABLE IF NOT EXISTS `user_login` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
-  `status` enum('0','1') DEFAULT NULL,
+  `status` enum('0','1') DEFAULT '1',
   `type` enum('a','b','c') DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `is_delete` enum('0','1') DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_delete` enum('0','1') DEFAULT '0',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
